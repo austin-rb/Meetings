fbdbd93b76aff1cd39b579a156d7105a38fb4e67
Nola Stowe <nola@rubygeek.com>
2012-09-27 07:05:29 -0500

    fixed typo

--- testing_stubs_for_katas/sample_minitest_spec.rb
+++ testing_stubs_for_katas/sample_minitest_spec.rb
@@ -3,7 +3,7 @@ require 'minitest/autorun'
 # Expectations:  http://bfts.rubyforge.org/minitest/MiniTest/Expectations.html
 
 describe "Trying Minitest spec" do
-  it "true should be tree" do
+  it "true should be true" do
     @boolean = true
     @boolean.must_equal true
   end
